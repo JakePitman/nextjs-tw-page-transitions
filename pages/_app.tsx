8665a6304b7cbe890c22dd2865f895247367b522
jakepitman <jakeseanp@gmail.com>
2022-12-26 12:35:42 +0900

    Add links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { AnimatePresence } from "framer-motion";
+
+type Props = {
+  Component: React.ElementType;
+  pageProps: {};
+  router: any;
+};
+
+const MyApp = ({ Component, pageProps, router }: Props) => {
+  return (
+    <div className="text-blue-900">
+      <AnimatePresence mode="wait" initial={false}>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default MyApp;
